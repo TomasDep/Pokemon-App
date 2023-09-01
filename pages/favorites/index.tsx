dd0d348c741ecd609e9f60b0506cf58c83fd9167
TomasDep <tomas.valenzuelacb@gmail.com>
2023-08-31 22:20:15 -0400

    ADD favorites pages

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@/components/layouts";
+
+const FavoritesPage = () => {
+  return (
+    <Layout title="Pokemons - Favorites">
+      <h1>Favorites</h1>
+    </Layout>
+  );
+};
+
+export default FavoritesPage;
