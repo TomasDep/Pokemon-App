20a139864b934b847683ac04abadb745c936caf3
TomasDep <tomas.valenzuelacb@gmail.com>
2023-08-31 22:19:18 -0400

    redirect in navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,34 +1,43 @@
 import { colors } from "@nextui-org/react";
 import Image from "next/image";
+import NextLink from "next/link";
+import { Link } from "@nextui-org/react";
 
 export const Navbar = () => {
-    const randNumber: number = Math.floor(Math.random() * (387 - 0 + 1) + 0);
-    return (
-        <div
-            style={{
-                display: "flex",
-                width: "100%",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                padding: "0px 20px",
-                backgroundColor: colors?.zinc[900],
-            }}
-        >
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <Image
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randNumber}.png`}
-                    alt="Pokemon icon"
-                    height={70}
-                    width={70}
-                />
-                <h1>
-                    <span className="text-2xl">P</span>
-                    <span className="text-xl">okémon</span>
-                </h1>
-            </div>
-
-            <h3 className="text-xl">Favorites</h3>
-        </div>
-    );
+  const randNumber: number = Math.floor(Math.random() * (151 - 0 + 1) + 0);
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0px 20px",
+        backgroundColor: colors?.zinc[900],
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randNumber}.png`}
+          alt="Pokemon icon"
+          height={70}
+          width={70}
+        />
+        <NextLink href="/">
+          <Link>
+            <h1>
+              <span className="text-2xl">P</span>
+              <span className="text-xl">okémon</span>
+            </h1>
+          </Link>
+        </NextLink>
+      </div>
+      <NextLink href="/favorites">
+        <Link>
+          <h3 className="text-xl">Favorites</h3>
+        </Link>
+      </NextLink>
+    </div>
+  );
 };
