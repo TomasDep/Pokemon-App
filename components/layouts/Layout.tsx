63b9a68e6b81b402e8d78bd2d099c61c5215c595
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-02 14:39:41 -0400

    ADD meta tags for seo

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -3,30 +3,42 @@ import Head from "next/head";
 import { Navbar } from "../ui";
 
 interface Props {
-    children: any;
-    title?: string;
+  children: any;
+  title?: string;
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
-    return (
-        <>
-            <Head>
-                <title>{title || "Pokemon App"}</title>
-                <meta name="author" content="TomasDep" />
-                <meta
-                    name="description"
-                    content={`Information about the pokemon ${title}`}
-                />
-                <meta name="keywords" content={`${title}, pokemon, pokedex`} />
-            </Head>
-            <Navbar />
-            <main
-                style={{
-                    padding: "0px 20px",
-                }}
-            >
-                {children}
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>{title || "Pokemon App"}</title>
+        <meta name="author" content="TomasDep" />
+        <meta
+          name="description"
+          content={`Information about the pokemon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta
+          property="og:title"
+          content={`Information about the pokemon ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`This page is about the pokemon ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main
+        style={{
+          padding: "20px 20px",
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
 };
