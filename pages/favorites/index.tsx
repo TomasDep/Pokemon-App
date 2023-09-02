cdddb956b155442f64d66f199181f63a1a4470d6
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-02 14:41:51 -0400

    UPD favorites index page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { Layout } from "@/components/layouts";
+import { NotFavorites } from "@/components/ui";
+import { localFavorites } from "@/utils";
+import { FavoritePokemons } from "@/components/pokemon";
 
 const FavoritesPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
   return (
     <Layout title="Pokemons - Favorites">
-      <h1>Favorites</h1>
+      {favoritePokemons.length === 0 ? (
+        <NotFavorites />
+      ) : (
+        <FavoritePokemons pokemons={favoritePokemons} />
+      )}
     </Layout>
   );
 };
